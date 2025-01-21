f8664e9a69cc2d1d106553d27ee6dae51e3d8c37
frnz1s <franzstevenlabrador@gmail.com>
2025-01-21 23:48:12 +0800

    added functionality to display winner

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ A simple dice game where two people choose to be either player 1 or player 2 and
 see who wins between the two.
 
 # Why make this game?
-1. To revise my fundamentals of HTML, CSS, and JavaScript, adhering to principles of modularity,
+1. To revise fundamentals of HTML, CSS, and JavaScript, adhering to principles of modularity,
 and reusability, as well as improving code readability.
 2. Dice is a fun game of chance
--- index.js
+++ index.js
@@ -6,14 +6,16 @@ function main() {
 }
 
 function rollTwoDice() {
-  var playerNum = [];
+  let playerNum = [];
   for (var i = 0; i < 2; i++) {
     // 1. roll dice number between 1 to 6
     var rolledNum = rollDie();
+    playerNum[i] = rolledNum;
     
     // 2. change image of dice 1, when it is rolled, and then dice 2
     changeDiceImage((i+1), rolledNum);
   }
+  displayWinner(playerNum);
 }
 
 function rollDie() {
@@ -22,7 +24,7 @@ function rollDie() {
 
 function getRandomNumBetween(min, max) {
   var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log("Random number = " + randomNum);
+  // console.log("Random number = " + randomNum);
   return randomNum;
 }
 
@@ -39,12 +41,24 @@ function changeDiceImage(dice_i, rolledNum) {
     return 0;
   }
   if (rolledNum < 1 || rolledNum > 6) {
-    console.error("Error: Rolled number should only be 1 to 6 my friend.");
+    console.error("Error: A regular die can only be rolled from 1 to 6 my friend.");
     return 0;
   }
   document.querySelector(".diceImg" + dice_i).setAttribute(
-    "src", ("./images/dice" + rolledNum + ".png")
+    "src", `./images/dice${rolledNum}.png`
   );
+}
+
+function displayWinner(playerNum) {
+  if (playerNum[0] > playerNum[1]) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+  }
+  else if (playerNum[1] > playerNum[0]) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
+  }
 }
 
 main();
